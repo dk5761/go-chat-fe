6296b496412be0a794d84c229b614015b9a19e8b
dk5761 <darshankadam714@gmail.com>
2024-11-08 12:59:20 +0530

    done with font setup

--- app/index.tsx
+++ app/index.tsx
@@ -6,16 +6,7 @@ type Props = {};
 const index = (props: Props) => {
   return (
     <View className=" flex-1  border-red-300 border-2 px-2 py-2 justify-center items-center">
-      <Text
-        style={{
-          // fontFamily: "",
-          fontSize: 74,
-
-          fontWeight: "900",
-        }}
-      >
-        Index
-      </Text>
+      <Text className="font-rubik-thin text-6xl">Index</Text>
     </View>
   );
 };
